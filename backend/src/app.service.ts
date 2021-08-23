8925198cb5fb82c43deda036400c5b5a2d4422e3
Sequoiageant <j.nolleau88@gmail.com>
2021-08-23 19:25:08 +0200

    vue.js on frontend working

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Backend: Hello World!';
+    return 'Backend: Hello World ' + process.env.NAME;
   }
 }
