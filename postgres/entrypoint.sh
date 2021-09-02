5f281d25c458106507a3999a337e340bd8945830
Sequoiageant <j.nolleau88@gmail.com>
2021-09-02 14:54:23 +0200

    delete postgres dockerfile

--- postgres/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER root WITH SUPERUSER PASSWORD 'password';
-    GRANT ALL PRIVILEGES ON DATABASE ft_transcendence_db TO root;
-EOSQL
