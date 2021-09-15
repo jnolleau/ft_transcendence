e9551156b9aa98ba1843b35e74081391e63ab69b
Sequoiageant <j.nolleau88@gmail.com>
2021-09-15 12:25:54 +0200

    hmr test with nestJs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as fs from "fs";
+// declare const module: any; // For Hot-Module Replacement
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     credentials: true,
     origin: ["http://localhost:8080", "http://127.0.0.1:8080"]
-  }); // Mandatory to interact w vue js which is on a different port
+  }); // Mandatory to interact w/ vue js which is on a different port
 
   //Setting up the OpenApi to generate client sdk
   const config = new DocumentBuilder()
@@ -24,6 +25,12 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
+  // For Hot-Module Replacement
+  // if (module.hot) {
+  //   module.hot.accept();
+  //   module.hot.dispose(() => app.close());
+  // }
+  ////////////////////////////////////////
 
   await app.listen(3000);
 }
