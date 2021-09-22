8a699b99bdf342efcb92b3da5cceae54fe89591c
Sequoiageant <j.nolleau88@gmail.com>
2021-09-22 22:18:04 +0200

    fix: getUsers now works properely

--- backend/src/scripts/getUsers.ts
+++ backend/src/scripts/getUsers.ts
@@ -1,23 +1,13 @@
-import { createConnection, createQueryBuilder, ConnectionOptions } from 'typeorm';
+import { createConnection, ConnectionOptions } from 'typeorm';
 import { Users } from '../users/entity/users.entity';
 import { configService } from '../config/config.service';
 
 async function run() {
   const connection = await createConnection(configService.getTypeOrmConfig() as ConnectionOptions)
-    .then(connection => {
-      createQueryBuilder()
-        .insert()
-        .into(Users)
-        .values([
-          { firstname: "Harry", lastname: "Potter", password: "password", salt: "salt" },
-          { firstname: "Ron", lastname: "Weasley", password: "password", salt: "salt" },
-          { firstname: "Hermione", lastname: "Granger", password: "password", salt: "salt" },
-          { firstname: "Severus", lastname: "Rogue", password: "password", salt: "salt" }
-        ])
-        .execute();
-    })
+  const userRepository = connection.getRepository(Users);
+  return userRepository.find();
 }
 
 run()
-  .then(_ => console.log('...wait for script to exit'))
-  .catch(error => console.error('delete error', error));
+  .then(users => console.log(users))
+  .catch(error => console.error('select error', error));
